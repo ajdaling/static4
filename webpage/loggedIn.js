14bc72fa42a5d8fbec0babc29e9765225e2c6837
ajdaling <ajdaling@gmail.com>
2017-10-17 16:00:12 -0400

    testing ajax call to 8504

--- webpage/loggedIn.js
+++ webpage/loggedIn.js
@@ -8,3 +8,12 @@ var cogUser = userPool.getCurrentUser();
 console.log(cogUser);
 
 $("#results").text("Hello, " + cogUser.username);
+
+$.ajax({
+  url:"http:68.68.26.91:8504/",
+  success:function(data){
+    alert('ajax success');
+    console.log(data);
+    $("#results2").text(JSON.stringify(data));
+  }
+});
